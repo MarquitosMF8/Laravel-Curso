fb2376cfd8d193e40afefd9a878376c471a5bccb
Marco Venegas <mvenegas@estrasol.com.mx>
2020-09-14 11:15:50 -0600

    #3 Routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('fotos/{numero?}',function($numero = 'sin número'){
+
+    return 'Estás en la galeria de fotos: '.$numero;
+})->where('numero', '[0-9]+');
+
+Route::view('galeria','fotos', [ 'numero'=> 125]);
